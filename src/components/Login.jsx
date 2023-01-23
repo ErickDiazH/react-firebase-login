6e3a403dbc9c966d036bafb774a3949a855605c5
Erick <ericksbas@gmail.com>
2023-01-22 21:44:35 -0700

    add tailwind styles

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useAuth } from "../context/authContext";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Alert from "./Alert";
 
 const Login = (props) => {
@@ -39,29 +39,56 @@ const Login = (props) => {
   };
 
   return (
-    <div>
+    <div className="w-full max-w-xs m-auto">
       {error && <Alert message={error} />}
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          name="email"
-          placeholder="youremail@example.com"
-          onChange={handleChange}
-        />
-
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          name="password"
-          id="password"
-          placeholder="* * * * * *"
-          onChange={handleChange}
-        />
-
-        <button>login</button>
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+      >
+        <div className="mb-4">
+          <label
+            htmlFor="email"
+            className="block text-gray-700 text-sm font-bold mb-2"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            name="email"
+            placeholder="youremail@example.com"
+            onChange={handleChange}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="password"
+            className="block text-gray-700 text-sm font-bold mb-2"
+          >
+            Password
+          </label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="* * * * * *"
+            onChange={handleChange}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline text-sm">
+          Login
+        </button>
       </form>
-      <button onClick={handleGoogleSigin}>Google login</button>
+      <p className="my-4 text-sm flex justify-between px-2">
+        Don't have an account? <Link to="/register">Register</Link>
+      </p>
+      <button
+        onClick={handleGoogleSigin}
+        className="bg-slate-50 hover:bg-slate-200 text-black shadow-md rounded border-2 border-gray-300 py-2 px-4 w-full"
+      >
+        Google login
+      </button>
     </div>
   );
 };
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useAuth } from "../context/authContext";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Alert from "./Alert";
 
 const Register = (props) => {
@@ -9,49 +9,71 @@ const Register = (props) => {
     password: "",
   });
 
-  const { signup } = useAuth()
-  const navigate = useNavigate()
+  const { signup } = useAuth();
+  const navigate = useNavigate();
 
   const [error, setError] = useState();
 
-  const handleChange = ({target: {name, value}}) => {
-    setUser({...user, [name]: value})
+  const handleChange = ({ target: { name, value } }) => {
+    setUser({ ...user, [name]: value });
   };
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
-    setError('')
+    e.preventDefault();
+    setError("");
     try {
-        await signup(user.email, user.password)
-        navigate('/')
+      await signup(user.email, user.password);
+      navigate("/");
     } catch (error) {
-        setError(error.message)
+      setError(error.message);
     }
-  }
+  };
 
   return (
-    <div>
-        {error && <Alert message={error} />}
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="email">Email</label>
+    <div className="w-full max-w-xs m-auto">
+      {error && <Alert message={error} />}
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+      >
+        <div className="mb-4">
+          <label
+            htmlFor="email"
+            className="block text-gray-700 text-sm font-bold mb-2"
+          >
+            Email
+          </label>
           <input
             type="email"
             name="email"
             placeholder="youremail@example.com"
             onChange={handleChange}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           />
-        
-          <label htmlFor="password">Password</label>
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="password"
+            className="block text-gray-700 text-sm font-bold mb-2"
+          >
+            Password
+          </label>
           <input
             type="password"
             name="password"
             id="password"
             placeholder="* * * * * *"
             onChange={handleChange}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           />
-        
-          <button>Register</button>
-        </form>
+        </div>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline text-sm">
+          Register
+        </button>
+      </form>
+      <p className="my-4 text-sm flex justify-between px-2">
+        Already have an account? <Link to="/login">Login</Link>
+      </p>
     </div>
   );
 };
